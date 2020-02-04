f7dd093edf15ccb9a5d2ddd93c066e88f9b0ecd1
Ayush Singh <ayushsingh1325@gmail.com>
2020-02-04 15:16:36 +0530

    feat: Updated qtile config

--- qtile/config.py
+++ qtile/config.py
@@ -27,7 +27,6 @@
 from libqtile.config import Key, Screen, Group, Drag, Click
 from libqtile.command import lazy
 from libqtile import layout, bar, widget, hook
-
 from typing import List  # noqa: F401
 import os
 import subprocess
@@ -37,124 +36,149 @@ mod = "mod4"
 # STARTUP APPLICATIONS
 @hook.subscribe.startup_once
 def start_once():
-    home = os.path.expanduser('~')
-    subprocess.call([home + '/.config/qtile/autostart.sh'])
+    home = os.path.expanduser("~")
+    subprocess.call([home + "/.config/qtile/autostart.sh"])
 
-keys = [
-    # Switch between windows in current stack pane
-    Key([mod], "k", lazy.layout.down()),
-    Key([mod], "j", lazy.layout.up()),
 
-    # Move windows up or down in current stack
-    Key([mod, "control"], "k", lazy.layout.shuffle_down()),
-    Key([mod, "control"], "j", lazy.layout.shuffle_up()),
+def init_keys():
+    return [
+        # Switch between windows in current stack pane
+        Key([mod], "k", lazy.layout.down()),
+        Key([mod], "j", lazy.layout.up()),
+        # Move windows up or down in current stack
+        Key([mod, "control"], "k", lazy.layout.shuffle_down()),
+        Key([mod, "control"], "j", lazy.layout.shuffle_up()),
+        # Switch window focus to other pane(s) of stack
+        Key([mod], "space", lazy.layout.next()),
+        # Swap panes of split stack
+        Key([mod, "shift"], "space", lazy.layout.rotate()),
+        # Toggle between split and unsplit sides of stack.
+        # Split = all windows displayed
+        # Unsplit = 1 window displayed, like Max layout, but still with
+        # multiple stack panes
+        Key([mod, "shift"], "Return", lazy.layout.toggle_split()),
+        Key([mod], "Return", lazy.spawn("urxvt")),
+        # Toggle between different layouts as defined below
+        Key([mod], "Tab", lazy.next_layout()),
+        Key([mod], "w", lazy.window.kill()),
+        Key([mod, "control"], "r", lazy.restart()),
+        Key([mod, "control"], "q", lazy.shutdown()),
+        Key([mod], "r", lazy.spawn("rofi -show run")),
+        # Sound
+        Key([], "XF86AudioMute", lazy.spawn("pamixer -t")),
+        Key([], "XF86AudioLowerVolume", lazy.spawn("pamixer -d 5 -u")),
+        Key([], "XF86AudioRaiseVolume", lazy.spawn("pamixer -i 5 -u")),
+    ]
 
-    # Switch window focus to other pane(s) of stack
-    Key([mod], "space", lazy.layout.next()),
 
-    # Swap panes of split stack
-    Key([mod, "shift"], "space", lazy.layout.rotate()),
-
-    # Toggle between split and unsplit sides of stack.
-    # Split = all windows displayed
-    # Unsplit = 1 window displayed, like Max layout, but still with
-    # multiple stack panes
-    Key([mod, "shift"], "Return", lazy.layout.toggle_split()),
-    Key([mod], "Return", lazy.spawn("urxvt")),
-
-    # Toggle between different layouts as defined below
-    Key([mod], "Tab", lazy.next_layout()),
-    Key([mod], "w", lazy.window.kill()),
-
-    Key([mod, "control"], "r", lazy.restart()),
-    Key([mod, "control"], "q", lazy.shutdown()),
-    Key([mod], "r", lazy.spawn('rofi -show run')),
-
-    # Sound
-    Key([], "XF86AudioMute", lazy.spawn("pamixer -t")),
-    Key([], "XF86AudioLowerVolume", lazy.spawn("pamixer -d 5 -u")),
-    Key([], "XF86AudioRaiseVolume", lazy.spawn("pamixer -i 5 -u"))
-]
-
-groups = [Group(i) for i in "asdfuiop"]
-
-for i in groups:
-    keys.extend([
-        # mod1 + letter of group = switch to group
-        Key([mod], i.name, lazy.group[i.name].toscreen()),
-
-        # mod1 + shift + letter of group = switch to & move focused window to group
-        Key([mod, "shift"], i.name, lazy.window.togroup(i.name)),
-    ])
-
-layouts = [
-    layout.Max(),
-    layout.Stack(num_stacks=2)
-]
-
-widget_defaults = dict(
-    font='sans',
-    fontsize=12,
-    padding=3,
-)
-extension_defaults = widget_defaults.copy()
-
-screens = [
-    Screen(
-        bottom=bar.Bar(
+def init_groups(ks):
+    groups = [Group(i) for i in "asdfuiop"]
+    for i in groups:
+        ks.extend(
             [
-                widget.GroupBox(),
-                widget.Prompt(),
-                widget.WindowName(),
-                widget.TextBox("default config", name="default"),
-                widget.Systray(),
-                widget.Clock(format='%Y-%m-%d %a %I:%M %p'),
-            ],
-            24,
+                # mod1 + letter of group = switch to group
+                Key([mod], i.name, lazy.group[i.name].toscreen()),
+                # mod1 + shift + letter of group = switch to & move focused window to group
+                Key([mod, "shift"], i.name, lazy.window.togroup(i.name)),
+            ]
+        )
+    return groups
+
+
+def init_screens():
+    return [
+        Screen(
+            top=bar.Bar(
+                [
+                    widget.GroupBox(
+                        margin_y=0,
+                        margin_x=0,
+                        borderwidth=1,
+                        active="#AA00FF",
+                        inactive="#4A148C",
+                        rounded=True,
+                        highlight_method="block",
+                        foreground="#4A148C",
+                        this_screen_border="#4A148C",
+                        this_current_screen_border="#EA80FC",
+                    ),
+                    widget.WindowName(fontsize=14, foreground="#64FFDA",),
+                    widget.Systray(),
+                    widget.Sep(),
+                    widget.Pacman(
+                        foreground="#76FF03", execute="urxvt -e sudo pacman -Syu",
+                    ),
+                    widget.Sep(),
+                    widget.CurrentLayout(foreground="#2962FF",),
+                    widget.Sep(),
+                    widget.TextBox(text=" 🕒 ", foreground="#D50000",),
+                    widget.Clock(foreground="#D32F2F", format="%A, %B %d - %H:%M",),
+                ],
+                28,
+            ),
         ),
-    ),
-]
+    ]
+
 
 # Drag floating layouts.
-mouse = [
-    Drag([mod], "Button1", lazy.window.set_position_floating(),
-         start=lazy.window.get_position()),
-    Drag([mod], "Button3", lazy.window.set_size_floating(),
-         start=lazy.window.get_size()),
-    Click([mod], "Button2", lazy.window.bring_to_front())
-]
+def init_mouse():
+    return [
+        Drag(
+            [mod],
+            "Button1",
+            lazy.window.set_position_floating(),
+            start=lazy.window.get_position(),
+        ),
+        Drag(
+            [mod],
+            "Button3",
+            lazy.window.set_size_floating(),
+            start=lazy.window.get_size(),
+        ),
+        Click([mod], "Button2", lazy.window.bring_to_front()),
+    ]
 
-dgroups_key_binder = None
-dgroups_app_rules = []  # type: List
-main = None
-follow_mouse_focus = True
-bring_front_click = False
-cursor_warp = False
-floating_layout = layout.Floating(float_rules=[
-    {'wmclass': 'confirm'},
-    {'wmclass': 'dialog'},
-    {'wmclass': 'download'},
-    {'wmclass': 'error'},
-    {'wmclass': 'file_progress'},
-    {'wmclass': 'notification'},
-    {'wmclass': 'splash'},
-    {'wmclass': 'toolbar'},
-    {'wmclass': 'confirmreset'},  # gitk
-    {'wmclass': 'makebranch'},  # gitk
-    {'wmclass': 'maketag'},  # gitk
-    {'wname': 'branchdialog'},  # gitk
-    {'wname': 'pinentry'},  # GPG key password entry
-    {'wmclass': 'ssh-askpass'},  # ssh-askpass
-])
-auto_fullscreen = True
-focus_on_window_activation = "smart"
 
-# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
-# string besides java UI toolkits; you can see several discussions on the
-# mailing lists, GitHub issues, and other WM documentation that suggest setting
-# this string if your java app doesn't work correctly. We may as well just lie
-# and say that we're a working one by default.
-#
-# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
-# java that happens to be on java's whitelist.
-wmname = "LG3D"
+def init_layouts():
+    return [layout.MonadTall(), layout.Max(), layout.Floating()]
+
+
+def init_folating_layout():
+    return layout.Floating(
+        float_rules=[
+            {"wmclass": "confirm"},
+            {"wmclass": "dialog"},
+            {"wmclass": "download"},
+            {"wmclass": "error"},
+            {"wmclass": "file_progress"},
+            {"wmclass": "notification"},
+            {"wmclass": "splash"},
+            {"wmclass": "toolbar"},
+            {"wmclass": "confirmreset"},  # gitk
+            {"wmclass": "makebranch"},  # gitk
+            {"wmclass": "maketag"},  # gitk
+            {"wname": "branchdialog"},  # gitk
+            {"wname": "pinentry"},  # GPG key password entry
+            {"wmclass": "ssh-askpass"},  # ssh-askpass
+        ]
+    )
+
+
+if __name__ in ["config", "__main__"]:
+    wmname = "LG3D"
+    auto_fullscreen = True
+    focus_on_window_activation = "smart"
+    follow_mouse_focus = True
+    bring_front_click = False
+    cursor_warp = False
+    main = None
+    widget_defaults = dict(font="Ubuntu Bold", fontsize=16, padding=3,)
+    extension_defaults = widget_defaults.copy()
+    dgroups_key_binder = None
+    dgroups_app_rules = []  # type: List
+    floating_layout = init_folating_layout()
+    layouts = init_layouts()
+    screens = init_screens()
+    keys = init_keys()
+    groups = init_groups(keys)
+    mouse = init_mouse()
