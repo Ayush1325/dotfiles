0db78255431fd47ccdaa385d9a059eb71a592a63
Ayush Singh <ayushsingh1325@gmail.com>
2020-03-13 19:28:42 +0530

    feat: Updated emacs config
    body: Moved custom variables to seperate files and added a whole bunch
    of other stuff

---
+++ .emacs.d/.emacs-custom.el
@@ -0,0 +1,31 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(counsel-projectile-mode t nil (counsel-projectile))
+ '(dired-hide-details-hide-information-lines t)
+ '(diredp-hide-details-initially-flag t)
+ '(diredp-hide-details-propagate-flag t)
+ '(global-dired-hide-details-mode t)
+ '(inhabit-startup-message t)
+ '(inhibit-startup-screen t)
+ '(markdown-enable-math t)
+ '(markdown-enable-wiki-links t)
+ '(markdown-fontify-code-blocks-natively t)
+ '(package-selected-packages
+   (quote
+    (ob-restclient restclient quelpa-use-package dired+ ibuffer-projectile adaptive-wrap vue-mode emojify company-emoji which-key use-package undo-tree try rust-mode rainbow-mode paredit org-bullets org-ac multiple-cursors magit lsp-ui lsp-ivy lorem-ipsum iedit highlight-parentheses highlight-defined helm expand-region eshell-prompt-extras esh-autosuggest edit-indirect dracula-theme doom-themes doom-modeline dashboard dap-mode counsel-projectile company-lsp)))
+ '(tool-bar-mode nil))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "UbuntuMono Nerd Font Mono" :foundry "DAMA" :slant normal :weight bold :height 203 :width normal))))
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0))))
+ '(markdown-header-face-1 ((t (:inherit markdown-header-face :foreground "violet red" :height 1.4))))
+ '(markdown-header-face-2 ((t (:inherit markdown-header-face :foreground "medium orchid" :height 1.3))))
+ '(markdown-header-face-3 ((t (:inherit markdown-header-face :foreground "chartreuse" :height 1.2))))
+ '(markdown-header-face-4 ((t (:inherit markdown-header-face :foreground "orange red" :height 1.1)))))
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,27 +1,17 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (highlight-defined paredit esh-autosuggest doom-themes rainbow-mode highlight-parentheses dashboard doom-modeline rust-mode dap-mode lsp-ivy company-lsp lsp-ui lsp-mode magit counsel-projectile projectile iedit multiple-cursors expand-region undo-tree dracula-theme company company-mode counsel consel ivy helm lorem-ipsum ace-window org-bullets try which-key use-package)))
- '(tool-bar-mode nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
-
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/"))
+	     '("melpa" . "https://melpa.org/packages/")
+	     '("elpa" . "https://elpa.gnu.org/packages"))
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
+(setq custom-file "~/.emacs.d/.emacs-custom.el")
+(load custom-file)
+
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+
+(put 'dired-find-alternate-file 'disabled nil)
