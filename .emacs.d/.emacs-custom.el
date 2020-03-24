b91d39abe99f07fa94e5e62c7df89cc8283b4367
Ayush Singh <ayushsingh1325@gmail.com>
2020-03-24 20:47:52 +0530

    feat: Added sx for stack overflow.

--- .emacs.d/.emacs-custom.el
+++ .emacs.d/.emacs-custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(aw-scope (quote frame))
  '(counsel-projectile-mode t nil (counsel-projectile))
  '(dired-hide-details-hide-information-lines t)
  '(dired-listing-switches "-alh")
@@ -16,7 +17,7 @@
  '(markdown-fontify-code-blocks-natively t)
  '(package-selected-packages
    (quote
-    (hungry-delete helm-company sunrise smart-hungry-delete pdf-view-restore pdf-tools engine-mode ob-restclient restclient quelpa-use-package dired+ ibuffer-projectile adaptive-wrap vue-mode emojify company-emoji which-key use-package undo-tree try rust-mode rainbow-mode paredit org-bullets org-ac multiple-cursors magit lsp-ui lsp-ivy lorem-ipsum iedit highlight-parentheses highlight-defined helm expand-region eshell-prompt-extras esh-autosuggest edit-indirect dracula-theme doom-themes doom-modeline dashboard dap-mode counsel-projectile company-lsp)))
+    (sx slime all-the-icons-ivy-rich ivy-rich hungry-delete helm-company sunrise smart-hungry-delete pdf-view-restore pdf-tools engine-mode ob-restclient restclient quelpa-use-package dired+ ibuffer-projectile adaptive-wrap vue-mode emojify company-emoji which-key use-package undo-tree try rust-mode rainbow-mode paredit org-bullets org-ac multiple-cursors magit lsp-ui lsp-ivy lorem-ipsum iedit highlight-parentheses highlight-defined helm expand-region eshell-prompt-extras esh-autosuggest edit-indirect dracula-theme doom-themes doom-modeline dashboard dap-mode counsel-projectile company-lsp)))
  '(sunrise-attributes-display-mask (quote (t nil nil nil nil)))
  '(tool-bar-mode nil))
 
