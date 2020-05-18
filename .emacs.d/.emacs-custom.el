2cb253aafdea08d5c2ee7eaf9d88fd6f6fb108e6
Ayush Singh <ayushsingh1325@gmail.com>
2020-05-18 11:16:01 +0530

    feat: Removed redundant stuff emacs config.

--- .emacs.d/.emacs-custom.el
+++ .emacs.d/.emacs-custom.el
@@ -4,10 +4,13 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(aw-scope (quote frame))
+ '(dart-format-on-save t t)
+ '(dart-sdk-path "~/flutter/bin/cache/dart-sdk/" t)
  '(dired-hide-details-hide-information-lines t)
  '(dired-listing-switches "-alh")
  '(diredp-hide-details-initially-flag t)
  '(diredp-hide-details-propagate-flag t)
+ '(flutter-sdk-path "~/flutter/" t)
  '(global-dired-hide-details-mode t)
  '(inhabit-startup-message t)
  '(inhibit-startup-screen t)
@@ -16,15 +19,16 @@
  '(markdown-fontify-code-blocks-natively t)
  '(package-selected-packages
    (quote
-    (move-text prettier-js emmet-mode web-mode company-restclient pipenv sx slime all-the-icons-ivy-rich ivy-rich hungry-delete helm-company sunrise smart-hungry-delete pdf-view-restore pdf-tools engine-mode ob-restclient restclient quelpa-use-package dired+ ibuffer-projectile adaptive-wrap vue-mode emojify company-emoji which-key use-package undo-tree try rust-mode rainbow-mode paredit org-bullets org-ac multiple-cursors magit lsp-ui lsp-ivy lorem-ipsum iedit highlight-parentheses highlight-defined helm expand-region eshell-prompt-extras esh-autosuggest edit-indirect dracula-theme doom-themes doom-modeline dashboard dap-mode counsel-projectile company-lsp)))
- '(sunrise-attributes-display-mask (quote (t nil nil nil nil))))
+    (skewer-mode js2-mode go-mode treemacs-projectile lsp-dart yaml-mode flutter dart-mode json-mode quelpa virtualenvwrapper exec-path-from-shell aweshell system-packages auto-package-update move-text prettier-js emmet-mode web-mode company-restclient pipenv sx slime all-the-icons-ivy-rich ivy-rich hungry-delete helm-company sunrise smart-hungry-delete pdf-view-restore pdf-tools engine-mode ob-restclient restclient quelpa-use-package dired+ ibuffer-projectile adaptive-wrap vue-mode emojify company-emoji which-key use-package undo-tree try rust-mode rainbow-mode paredit org-bullets org-ac multiple-cursors magit lsp-ui lsp-ivy lorem-ipsum iedit highlight-parentheses highlight-defined helm expand-region eshell-prompt-extras esh-autosuggest edit-indirect dracula-theme doom-themes doom-modeline dashboard dap-mode counsel-projectile company-lsp)))
+ '(sunrise-attributes-display-mask (quote (t nil nil nil nil)))
+ '(tool-bar-mode nil))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "UbuntuMono Nerd Font Mono" :foundry "DAMA" :slant normal :weight bold :height 203 :width normal))))
+ '(default ((t (:family "JetBrainsMono Nerd Font Mono" :foundry "JB  " :slant normal :weight bold :height 181 :width normal))))
  '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0))))
  '(markdown-header-face-1 ((t (:inherit markdown-header-face :foreground "violet red" :height 1.4))))
  '(markdown-header-face-2 ((t (:inherit markdown-header-face :foreground "medium orchid" :height 1.3))))
