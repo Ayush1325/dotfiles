d8ad687e1378224eaa96dc47bcbca3c98397c5a3
Ayush Singh <ayushdevel1325@gmail.com>
2024-02-06 22:25:00 +0530

    Add zsh-autoswitch-virtualenv
    Signed-off-by: Ayush Singh <ayushdevel1325@gmail.com>

--- zsh/base.zsh
+++ zsh/base.zsh
@@ -10,6 +10,7 @@ path+=("$HOME/.local/bin")
 PLUGINS_PATH=$HOME/.config/zsh/plugins
 source $PLUGINS_PATH/zsh-autosuggestions/zsh-autosuggestions.zsh
 source $PLUGINS_PATH/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source $PLUGINS_PATH/zsh-autoswitch-virtualenv/autoswitch_virtualenv.plugin.zsh
 
 # Theme
 source $PLUGINS_PATH/powerlevel10k/powerlevel10k.zsh-theme
