b291632cf0ac2f6c94280afc955b3aa4000a26ab
Ayush Singh <ayush@beagleboard.org>
2024-12-20 18:59:58 +0530

    config: nvim: Update deps
    Signed-off-by: Ayush Singh <ayush@beagleboard.org>

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -12,3 +12,14 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins")
+
+-- Fix for rust-analyzer: https://github.com/neovim/neovim/issues/30985
+for _, method in ipairs({ "textDocument/diagnostic", "workspace/diagnostic" }) do
+    local default_diagnostic_handler = vim.lsp.handlers[method]
+    vim.lsp.handlers[method] = function(err, result, context, config)
+        if err ~= nil and err.code == -32802 then
+            return
+        end
+        return default_diagnostic_handler(err, result, context, config)
+    end
+end
